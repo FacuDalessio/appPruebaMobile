3c7b83217ee5bc5c287a778307fbd110f67bc239
Facundo Dalessio <fdalessio22@gmail.com>
2025-04-06 02:13:15 -0300

    envioronment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCY8bZbXnj7MG5qFEEaL2oEpUHl9qsScDQ",
+    authDomain: "pruebaapp-bf105.firebaseapp.com",
+    projectId: "pruebaapp-bf105",
+    storageBucket: "pruebaapp-bf105.firebasestorage.app",
+    messagingSenderId: "839903151975",
+    appId: "1:839903151975:web:c02eb56ca3b76430f751c2"
+  }
 };
